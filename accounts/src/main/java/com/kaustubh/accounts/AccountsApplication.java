4dc5fbd21b5f3ba4f8a089755c328c078d2281d8
Kaustubh Sharma <Kaustubh.Sharma2@standardbank.co.za>
2024-06-27 16:11:41 +0200

    first commit- accounts functions

--- accounts/src/main/java/com/kaustubh/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/kaustubh/accounts/AccountsApplication.java
@@ -1,9 +1,35 @@
 package com.kaustubh.accounts;
 
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.kaustubh.accounts.controller") })
+@EnableJpaRepositories("com.kaustubh.accounts.repository")
+@EntityScan("com.kaustubh.accounts.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "",
+				version = "v1",
+				contact = @Contact(
+						name = "Kaustubh"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  ""
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
